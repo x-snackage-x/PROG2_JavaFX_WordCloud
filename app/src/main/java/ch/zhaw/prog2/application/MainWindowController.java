97db59367056710aeb0f5ed7b380afe7b8321bf0
elhaykar <elhaykar@students.zhaw.ch>
2022-03-25 16:19:23 +0100

    Solved Aufgabe 1

--- app/src/main/java/ch/zhaw/prog2/application/MainWindowController.java
+++ app/src/main/java/ch/zhaw/prog2/application/MainWindowController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class MainWindowController {
+public class MainWindowController implements IsObserver{
 
     @FXML
     private Label lableTitel;
@@ -17,13 +17,24 @@ public class MainWindowController {
     @FXML
     private TextArea textHistory;
     
+    private WordModel model;
+    
+    private WordModelDecorator observableModel;
+    
     public void initialize() {
     	connectProperties();
+    	model = new WordModel();
+    	observableModel = new WordModelDecorator(model);
+    	observableModel.addListener(this);
     }
 
     @FXML
     void hinzufuegenText(ActionEvent event) {
-    	textHistory.setText(textEingabe.getText() + System.lineSeparator() + textHistory.getText());
+    	//textHistory.setText(textEingabe.getText() + System.lineSeparator() + textHistory.getText());
+    	for(String word : textEingabe.getText().split(" ")) {
+    		observableModel.addWord(word.toLowerCase());
+    	}
+    	textEingabe.clear();
     }
 
     @FXML
@@ -34,5 +45,10 @@ public class MainWindowController {
     private void connectProperties() {
     	lableTitel.textProperty().bind(textEingabe.textProperty());
     }
+
+	@Override
+	public void update() {
+		textHistory.setText(model.toString());
+	}
 
 }
